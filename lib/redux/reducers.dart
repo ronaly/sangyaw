5a96d17e70fa6c77409093fb9597a05d59c1f309
Bong Agdeppa <leopoldo.agdeppa@gmail.com>
2020-09-01 08:46:44 +0800

    updates for appstate

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -1,8 +1,5 @@
 
-import 'package:flutter/cupertino.dart';
-import 'package:redux/redux.dart';
 import 'package:sangyaw_app/model/app_state.dart';
-import 'package:sangyaw_app/model/person.dart';
 import 'package:sangyaw_app/redux/actions.dart';
 //
 //
