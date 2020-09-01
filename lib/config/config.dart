a3f2b19cba19409df64497f798b6e73fe4c34bd8
ronaly <ronaly@gmail.com>
2020-09-02 07:27:06 +0800

    Added Staging spreadsheet id in config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,4 +3,5 @@ const String APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxMqHh-lc
 const Map<String, String> SANGYAW_SHEET_IDS = {
   'Pamutan' : '1y-GqdmM20Byli_u-wVB1CbRzUDV0qPhdcb5fH5xowTU',
   'Tuong' : '1Dqt2yNeMH-KTORX36-evAfsl-ftXtNuML5r7ZSwD0fs',
+  'Staging': '11O-DdnPkUTfGmUsZ0jpPNE9GXHuGTFJQu7TKakr8IzE',
 };
