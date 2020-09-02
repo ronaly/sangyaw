6e518603f578775e817da4f23a4595cc44394b6a
Bong Agdeppa <leopoldo.agdeppa@gmail.com>
2020-09-02 11:18:20 +0800

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sangyaw_app/test_main.dart';
+import 'package:sangyaw_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
