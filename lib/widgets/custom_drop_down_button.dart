aaabbd30cd7a8929da56d1df088d05d2e46bb117
ronaly <ronaly@gmail.com>
2020-09-03 11:03:55 +0800

    fixed error in messenger status dropdown

--- lib/widgets/custom_drop_down_button.dart
+++ lib/widgets/custom_drop_down_button.dart
@@ -4,12 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:sangyaw_app/model/app_state.dart';
 import 'package:sangyaw_app/redux/actions.dart';
-//import 'package:sangyaw_app/utils/custom_dropdown_items.dart';
+import 'package:sangyaw_app/utils/custom_dropdown_items.dart';
 
 
 import 'app_stateful_widget.dart';
 
-final int whichWidget = 0;   // 0=gender, 1=ageGroup, 2=messengerstatus, 3=progresstatus
+//final int whichWidget = 0;   // 0=gender, 1=ageGroup, 2=messengerstatus, 3=progresstatus
 
 
 
@@ -20,79 +20,67 @@ class CustomDropDownButton extends StatefulWidget {
 }
 
 class _CustomDropDownButton extends AppStatefulWidget<CustomDropDownButton> {
-   int _dropdownValue;
-
-   @override
-   void initState() {
-     super.initState();
-     _dropdownValue = 1;
-     if (this.dc.currentPerson.messengerStatus == "Active") { _dropdownValue = 2;}
-     if (this.dc.currentPerson.messengerStatus == "Inactive") { _dropdownValue = 3;}
-   }
-
-
 
 
     @override
     Widget buildBody(BuildContext context) {
 
+      int val = 1;
+      if (this.dc.currentPerson.messengerStatus == "Active") {
+        val = 2;
+      }
+      if (this.dc.currentPerson.messengerStatus == "Inactive") {
+        val = 3;
+      }
+
+
 
       return DropdownButton(
-          value: _dropdownValue,
+          value: val,
           icon: Icon(Icons.arrow_downward),
           iconSize: 18,
           elevation: 16,
           style: TextStyle(color: Colors.black),
           items:
           messengerDropdownItems(),
-
-          // [
-          //   DropdownMenuItem(
-          //     child: Text(""),
-          //     value:1,
-          //   ),
-          //   DropdownMenuItem(
-          //     child: Text('Active'),
-          //     value:2,
-          //   ),
-          //
-          //   DropdownMenuItem(
-          //     child: Text('Inactive'),
-          //     value:3,
-          //   ),
-          // ],
           onChanged:
                (int newValue) {
              setState((){
-               _dropdownValue = newValue;
-                print("dropdownValue = $_dropdownValue");
+               switch(newValue) {
+                 case 2 :
+                   this.dc.currentPerson.messengerStatus = "Active";
+                   break;
+                 case 3 :
+                   this.dc.currentPerson.messengerStatus = "Inactive";
+                   break;
+                 default :
+                   this.dc.currentPerson.messengerStatus = "";
+               }
               });
-
             },
-
       ); //DropdownButton
 
     }  //widget build
 
-   List<DropdownMenuItem<int>> messengerDropdownItems() {
-     return (
-         [
-           DropdownMenuItem(
-             child: Text(""),
-             value:1,
-           ),
-           DropdownMenuItem(
-             child: Text('Active'),
-             value:2,
-           ),
-           DropdownMenuItem(
-             child: Text('Inactive'),
-             value:3,
-           ),
-
-         ]
-     ).toList();
-   }
+   // List<DropdownMenuItem<int>> messengerDropdownItems() {
+   //   return (
+   //       [
+   //         DropdownMenuItem(
+   //           child: Text(""),
+   //           value:1,
+   //         ),
+   //         DropdownMenuItem(
+   //           child: Text('Active'),
+   //           value:2,
+   //         ),
+   //         DropdownMenuItem(
+   //           child: Text('Inactive'),
+   //           value:3,
+   //         ),
+   //
+   //       ]
+   //   ).toList();
+   // }
 
 
 } //class
