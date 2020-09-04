5849dc77bc5a6d342062b9f07580dec1a6a7fabc
ronaly <ronaly@gmail.com>
2020-09-04 20:57:49 +0800

    fontstyle

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,15 +14,42 @@ class Home extends AppStatelessLayoutContainer {
   Widget buildBody(context, AppState state) {
 
     // START BODY HERE
-    Widget body = RichText(
-      text: TextSpan(
-        text: theText,
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.lightBlue,
-        ),
-      ),
+    // Widget body = RichText(
+    //   text: TextSpan(
+    //     text: theText,
+    //     style: TextStyle(
+    //       fontSize: 20,
+    //       color: Colors.lightBlue,
+    //     ),
+    //   ),
+    // );
+
+Widget body =    Container (
+      alignment: Alignment.center,
+      child: Text('Manangyaw Na Ta',
+      style: TextStyle(
+          fontSize: 40,
+          foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 2
+          ..color = Colors.blue[700],
+      ),),
     );
+
+
+
+    //   Widget body = AnimatedDefaultTextStyle(
+    //   duration: const Duration(milliseconds: 300),
+    //   style: TextStyle(
+    //     fontSize: 40,
+    //     color: Colors.blueAccent,
+    //     fontWeight: FontWeight.bold,
+    //     fontFamily: "Canterbury",
+    //   ),
+    //   child: Text('Manangyaw  na  ta!'),
+    // );
+
+
 
     // END/RETURN The body
     return body;
