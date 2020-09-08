0fb0375c9a1920b62b6395454ccd7bd628498362
Bong Agdeppa <leopoldo.agdeppa@gmail.com>
2020-09-08 19:05:09 +0800

    batch all person

--- lib/pages/batch_update_all_persons.dart
+++ lib/pages/batch_update_all_persons.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:sangyaw_app/model/person.dart';
+import 'package:sangyaw_app/widgets/app_stateful_layout_container.dart';
 import 'package:sangyaw_app/widgets/app_stateless_layout_container.dart';
 import 'package:sangyaw_app/model/app_state.dart';
 import 'package:sangyaw_app/widgets/person_list.dart';
 import 'package:sangyaw_app/widgets/select_person_list.dart';
 
-class BatchUpdateAllPersons extends AppStatelessLayoutContainer {
+
+
+class BatchUpdateAllPersons extends StatefulWidget {
+  @override
+  _BatchUpdateAllPersons createState() => new _BatchUpdateAllPersons();
+}
+
+
+
+class _BatchUpdateAllPersons extends  AppStatefulLayoutContainer<BatchUpdateAllPersons>  {
 
   @override
   String getTitle(context, AppState state) {
@@ -21,7 +31,7 @@ class BatchUpdateAllPersons extends AppStatelessLayoutContainer {
   Widget buildBody(context, AppState state) {
 
     // START BODY HERE
-    Widget body = SelectPersonList(list: this.dc.persons, selectedMap: {},onPersonSelect: this.onPersonSelect, );
+    Widget body = SelectPersonList(list: this.dc.persons, selectedMap: {20: true},onPersonSelect: this.onPersonSelect, );
 
     // END/RETURN The body
     return body;
